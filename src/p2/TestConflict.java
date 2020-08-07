1b9308923bdf3da855e12e60f380f6f59f22049b
Amita Rout <amitarout11@gmail.com>
2020-08-07 15:56:07 -0400

    Added B

--- src/p2/TestConflict.java
+++ src/p2/TestConflict.java
@@ -4,6 +4,9 @@ public class TestConflict {
 	
 	public static void main(String[] args) {
 		System.out.println("In Conflict Branch A");
+		System.out.println("In Conflict Branch B using for loop");
+		System.out.println(100+200);
+		System.out.println("Hello");
 	}
 
 }
