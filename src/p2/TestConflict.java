108e81e1d9498ec2b820d9ea751cbefe7750815c
Amita Rout <amitarout11@gmail.com>
2020-08-07 15:47:16 -0400

    Added A again

--- src/p2/TestConflict.java
+++ src/p2/TestConflict.java
@@ -4,6 +4,7 @@ public class TestConflict {
 	
 	public static void main(String[] args) {
 		System.out.println("In Conflict Branch A");
+		System.out.println("In Conflict Branch A using While loop");
 	}
 
 }
